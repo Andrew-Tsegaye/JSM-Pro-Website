8fc675d48f60e41b40fd246c4402236c0a0f525a
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2024-05-27 13:04:14 +0300

    backend setup

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -1,4 +1,4 @@
-interface BuildQuearyParams {
+interface BuildQueryParams {
   type: string;
   query: string;
   category: string;
@@ -6,26 +6,24 @@ interface BuildQuearyParams {
   perPage?: number;
 }
 
-export function buildQuery(params: BuildQuearyParams) {
-  const { type, query, category, page = 1, perPage = 10 } = params;
+export function buildQuery(params: BuildQueryParams) {
+  const { type, query, category, page = 1, perPage = 20 } = params;
 
-  const conditions = [`*[_type=="${type}"]`];
+  const conditions = [`*[_type=="${type}"`];
 
-  if (query) {
-    conditions.push(`title match "*${query}*"`);
-  }
+  if (query) conditions.push(`title match "*${query}*"`);
 
   if (category && category !== "all") {
     conditions.push(`category == "${category}"`);
   }
 
-  // calculate pagination limits
+  // Calculate pagination limits
   const offset = (page - 1) * perPage;
   const limit = perPage;
 
-  if (conditions.length > 1) {
-    return `${conditions[0]} && (${conditions.slice(1).join(" && ")})][${offset}...${limit}]`;
-  } else {
-    return `${conditions[0]}[${offset}...${limit}]`;
-  }
+  return conditions.length > 1
+    ? `${conditions[0]} && (${conditions
+        .slice(1)
+        .join(" && ")})][${offset}...${limit}]`
+    : `${conditions[0]}][${offset}...${limit}]`;
 }
